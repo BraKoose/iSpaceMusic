9d2223b2283a2a2723a731727396854bd3655026
Koose <KooseTheBlogger@gmail.com>
2022-01-25 21:04:26 +0000

    data class on music

--- app/src/main/java/com/koose/musicplay/MainActivity.kt
+++ app/src/main/java/com/koose/musicplay/MainActivity.kt
@@ -7,27 +7,11 @@ import android.widget.Button
 import android.widget.ImageButton
 
 class  MainActivity : AppCompatActivity() {
-    private lateinit var playbutton: ImageButton
+    private var playbutton: ImageButton = findViewById(R.id.play_btn)
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //first i am going to create MediaPlayer object
-        val mediaplayer:MediaPlayer = MediaPlayer.create(this, R.raw.music)
-
-        //now lets create our play button event
-        playbutton.setOnClickListener {
-            if (!mediaplayer.isPlaying){
-                mediaplayer.start()
-
-                playbutton.setImageResource(R.drawable.ic_baseline_play_circle_outline_24)
-            }else{
-                //the media player
-            }
-        }
-
-
 
     }
 }
---
+++ app/src/main/java/com/koose/musicplay/Model/Music.kt
@@ -0,0 +1,3 @@
+package com.koose.musicplay.Model
+
+data class Music(val song: String, val audioFile:Int)
