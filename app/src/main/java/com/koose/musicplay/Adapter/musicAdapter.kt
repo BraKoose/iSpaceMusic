9c329a592e9dc92239300bdfacac12c40a3a90a5
Koose <KooseTheBlogger@gmail.com>
2022-01-25 21:14:26 +0000

    adpater created and members implemented

---
+++ app/src/main/java/com/koose/musicplay/Adapter/musicAdapter.kt
@@ -0,0 +1,25 @@
+package com.koose.musicplay.Adapter
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.koose.musicplay.Model.Music
+
+class musicAdapter(var arrayList:ArrayList<Music> ) : BaseAdapter() {
+    override fun getCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItem(p0: Int): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemId(p0: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        TODO("Not yet implemented")
+    }
+
+}
