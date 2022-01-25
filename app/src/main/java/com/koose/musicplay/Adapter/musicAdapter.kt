cf1a1a6c77850024d1726f870cbd413a4a865522
Koose <KooseTheBlogger@gmail.com>
2022-01-25 21:39:42 +0000

    returned the neccesary getCount, and views

--- app/src/main/java/com/koose/musicplay/Adapter/musicAdapter.kt
+++ app/src/main/java/com/koose/musicplay/Adapter/musicAdapter.kt
@@ -1,25 +1,35 @@
 package com.koose.musicplay.Adapter
 
+import android.content.Context
+import android.media.MediaPlayer
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import com.koose.musicplay.Model.Music
 
 class musicAdapter(var arrayList:ArrayList<Music> ) : BaseAdapter() {
+
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return arrayList.size
     }
 
-    override fun getItem(p0: Int): Any {
-        TODO("Not yet implemented")
+    override fun getItem(position: Int): Any {
+        return arrayList[position]
     }
 
-    override fun getItemId(p0: Int): Long {
-        TODO("Not yet implemented")
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
     }
 
-    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        TODO("Not yet implemented")
+    override fun getView(position:Int, convertView: View?, parent: ViewGroup?): View {
+        val context = parent?.context
+        var rowView: View? =convertView
+        var mediaPlayer: MediaPlayer? = null
+
+        val inflate:LayoutInflater=context?.getSystemService(
+            Context.LAYOUT_INFLATER_SERVICE)as LayoutInflater
+        )
     }
 
 }
